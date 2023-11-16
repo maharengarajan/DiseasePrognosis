569ad997a2cb9cffead6fbb7a3ae52e989aadc19
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-11-16 13:30:16 +0530

    data ingestion stage added and tested successfully

---
+++ main.py
@@ -0,0 +1,13 @@
+from diseaseprognosis.pipeline import DataIngestionTrainingPipeline
+from diseaseprognosis import logger
+
+
+STAGE_NAME = "Data Ingestion"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
