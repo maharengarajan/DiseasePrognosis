499597ca1d64da18e9be10da2c6912fc12aad406
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-12-16 19:51:30 +0530

    entity updated for modelTrainer

--- src/diseaseprognosis/entity/config_entity.py
+++ src/diseaseprognosis/entity/config_entity.py
@@ -20,3 +20,15 @@ class DataValidationConfig:
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    n_estimators: int
+    min_samples_split: int
+    min_samples_leaf: int
+    random_state: int
+    target_column: str
