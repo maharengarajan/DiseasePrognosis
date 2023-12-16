9e598622213f44ee393ea352883399317237d5e6
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-12-16 21:24:14 +0530

    config manager added

--- src/diseaseprognosis/config/configuration.py
+++ src/diseaseprognosis/config/configuration.py
@@ -1,6 +1,6 @@
 from diseaseprognosis.utils import read_yaml, create_directories
 from diseaseprognosis.constants import *
-from diseaseprognosis.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from diseaseprognosis.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 
 
 class ConfigurationManager:
@@ -80,3 +80,23 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.RandomForest
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/maharengarajan/DiseasePrognosis.mlflow",     
+        )
+
+        return model_evaluation_config
