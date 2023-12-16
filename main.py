4d76c059ee250e006d4765fe811060fa7326104f
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-12-16 19:54:27 +0530

    ModelTrainer updated&Model created

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from diseaseprognosis.pipeline import DataIngestionTrainingPipeline
 from diseaseprognosis.pipeline import DataValidationTrainingPipeline
 from diseaseprognosis.pipeline import DataTransformationTrainingPipeline
+from diseaseprognosis.pipeline import ModelTrainerTrainingPipeline
 from diseaseprognosis import logger
 
 
@@ -35,3 +36,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
