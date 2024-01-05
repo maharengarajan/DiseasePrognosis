65bff7118a09120f1e70d14ad9ca661dd33ae439
RENGA RAJAN K <maharengarajan46@gmail.com>
2024-01-05 16:04:51 +0530

    flake8 installed & tested

--- src/diseaseprognosis/pipeline/prediction.py
+++ src/diseaseprognosis/pipeline/prediction.py
@@ -1,15 +1,12 @@
-import joblib 
-import numpy as np
-import pandas as pd
+import joblib
 from pathlib import Path
 
 
 # load the model & predict
 class PredictionPipeline:
     def __init__(self):
-        self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
+        self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))
 
-    
     def predict(self, data):
         prediction = self.model.predict(data)
 
